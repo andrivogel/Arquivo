2661da260ad3cc411b4eb0bafe8ef8102a853a5a
andrivogel <39415436+andrivogel@users.noreply.github.com>
2018-06-08 09:10:12 -0300

    Add files via upload

---
+++ 123/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="produtos.php?opcao=link1">link 1</a>
+<a href="produtos.php?opcao=link2">link 2</a>
+<a href="produtos.php?opcao=link3">link 3</a>
+<a href="produtos.php?opcao=link4">link 4</a>
+
+
+<form method="post" action="produtos.php?opcao=busca">
+    <input type="search" placeholder="busca">
+    <input type="submit" value="Enviar">
+</form>
+
+
+
+
+</body>
+</html>
---
+++ 123/produtos.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_GET['opcao'] == "link1"){
+    echo "Link 1";
+}
+
+elseif ($_GET['opcao'] == "link2"){
+    echo "Link 2";
+}
+
+elseif ($_GET['opcao'] == "link3"){
+    echo "Link 3";
+}
+elseif ($_GET['opcao'] == "link4"){
+    echo "Link 4";
+}
+
+elseif ($_GET['opcao'] == "busca"){
+    echo "Busca";
+}
+?>
